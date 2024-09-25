ac7e8719544b7d5d7bfbefdfd5b27b8cb64466bf
Rifat Sakib <sakibrifat353@gmail.com>
2024-09-25 10:57:40 +0600

    validation part done

--- js/donation.js
+++ js/donation.js
@@ -7,17 +7,21 @@ document.getElementById("donat1-btn").addEventListener("click", function (event)
 
 
     const donatAmountMoney = money("donate-amount1");
-    console.log(donatAmountMoney);
 
     const instituteBalance = balance("institute-balance1");
-    console.log(instituteBalance);
 
-    const addResult = donatAmountMoney + instituteBalance;
-
-
-    console.log(addResult);
+    const mainBalance = balance("balance");
 
 
+     //   validation part start
+    const validResult = validation(donatAmountMoney, mainBalance);
+
+   //   validation part end
+
+
+
+
+    const addResult = validResult[0] + instituteBalance;
 
     document.getElementById("institute-balance1").innerText = addResult;
 
@@ -26,10 +30,9 @@ document.getElementById("donat1-btn").addEventListener("click", function (event)
 
     // subtract part
 
-    const withdrawAmount = balance("balance");
-    console.log(withdrawAmount);
 
-    const subtractResult = withdrawAmount - donatAmountMoney;
+
+    const subtractResult = validResult[1] - validResult[0];
 
     document.getElementById("balance").innerText = `${subtractResult} BDT`;
 
@@ -47,72 +50,83 @@ document.getElementById("donat2-btn").addEventListener("click", function (event)
     event.preventDefault();
 
 
+    const mainBalance = balance("balance");
 
 
+   
     const donatAmountMoney = money("donate-amount2");
-    // console.log(donatAmountMoney);
 
     const instituteBalance = balance("institute-balance2");
-    // console.log(instituteBalance);
-
-    const addResult = donatAmountMoney + instituteBalance;
 
 
-    // console.log(addResult);
+
+     //   validation part start
+    const validResult = validation(donatAmountMoney, mainBalance);
+
+   //   validation part end
 
 
+
+
+    const addResult = validResult[0] + instituteBalance;
 
     document.getElementById("institute-balance2").innerText = addResult;
 
-    
+
+
+
     // subtract part
 
-    const withdrawAmount = balance("balance");
-    console.log(withdrawAmount);
 
-    const subtractResult = withdrawAmount - donatAmountMoney;
+
+    const subtractResult = validResult[1] - validResult[0];
 
     document.getElementById("balance").innerText = `${subtractResult} BDT`;
+
+
+
 
 });
 
 
 
 
-    //for Third donation
+//for Third donation
 
 
 document.getElementById("donat3-btn").addEventListener("click", function (event) {
     event.preventDefault();
 
+    const mainBalance = balance("balance");
 
 
 
     const donatAmountMoney = money("donate-amount3");
-    // console.log(donatAmountMoney);
 
     const instituteBalance = balance("institute-balance3");
-    // console.log(instituteBalance);
-
-    const addResult = donatAmountMoney + instituteBalance;
 
 
-    // console.log(addResult);
+
+     //   validation part start
+    const validResult = validation(donatAmountMoney, mainBalance);
+
+   //   validation part end
 
 
+
+
+    const addResult = validResult[0] + instituteBalance;
 
     document.getElementById("institute-balance3").innerText = addResult;
 
 
 
-       // subtract part
+
+    // subtract part
 
 
 
-    const withdrawAmount = balance("balance");
-    console.log(withdrawAmount);
-
-    const subtractResult = withdrawAmount - donatAmountMoney;
+    const subtractResult = validResult[1] - validResult[0];
 
     document.getElementById("balance").innerText = `${subtractResult} BDT`;
 
---
+++ js/validation.js
@@ -0,0 +1,30 @@
+function validation(donation, balance) {
+
+
+    if(balance < 0 || balance < donation){
+        alert("You dont have enough funds to make this transaction");
+
+        return;
+
+    }
+
+    if(isNaN(donation) || donation < 0){
+        alert('please enter a valid donation amount');
+        return;
+    }
+
+    else {
+
+       const values = [donation,balance]
+
+        return values;
+
+
+
+
+
+    }
+
+
+
+}
